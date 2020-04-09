64d9fad204badd07a02cbd98cec2fe0bd54a07ca
cmokaya94 <56549200+cmokaya94@users.noreply.github.com>
2020-04-08 22:09:48 -0400

    changes

--- app.py
+++ app.py
@@ -19,26 +19,25 @@ app = Flask(__name__)
 def zillow():
     if request.method == "POST":
          address = request.form['address']
-         postal = request.form['Zip Code']
+         postal = request.form['zipcode']
          print(address)
          print(postal)
          #Function to call data from Zillow and return dataframe
 
          def address_search(address, postal_code):
-         request_data = api.GetSearchResults(key, address, postal_code)
-         result = request_data.get_dict()
-         address = result['full_address']
+             request_data = api.GetSearchResults(key, address, postal_code)
+             result = request_data.get_dict()
+             address = result['full_address']
     
-        zestimate = result['zestimate']
-        comb_dict = dict(zestimate)
-        comb_dict.update(address)
-        combined_df = pd.DataFrame.from_dict(comb_dict, orient='index').T
-
-        combined_df['risk_adj_value'] = (combined_df['amount']*.85) #Risk adjusting home by 15%, reducing home val by 15%
-        combined_df['max_cash_limit'] = combined_df['risk_adj_value']*.30 #Cannot buy more than 30% of someones home equity, bc then youre pushing majority ownership
-        combined_df.to_sql(name='zillow', con=engine, if_exists='append', index=False)
+             zestimate = result['zestimate']
+             comb_dict = dict(zestimate)
+             comb_dict.update(address)
+             combined_df = pd.DataFrame.from_dict(comb_dict, orient='index')
+             combined_df['risk_adj_value'] = (combined_df['amount']*.85) #Risk adjusting home by 15%, reducing home val by 15%
+             combined_df['max_cash_limit'] = combined_df['risk_adj_value']*.30 #Cannot buy more than 30% of someones home equity, bc then youre pushing majority ownership
+             combined_df.to_sql(name='zillow', con=engine, if_exists='append', index=False)
     
-        return(combined_df)
+             return(combined_df)
 
 
 
