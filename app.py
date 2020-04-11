92bbe2822d472ffa338fa17a5688e038050e8231
cmokaya94 <56549200+cmokaya94@users.noreply.github.com>
2020-04-11 14:00:06 -0400

    sIGN UP

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ from calculations import address_search
 from config import zillow_key, database_key
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField,IntegerField
+from wtforms.validators import InputRequired, Email, Length
+from flask_bootstrap import Bootstrap
+from sqlalchemy.ext.automap import automap_base
+
+
 
 
 
@@ -19,10 +24,17 @@ app = Flask(__name__)
 
 connection_string = database_key
 engine = create_engine(f'postgresql://{connection_string}')
+
+conn=engine.connect()
+
+print(conn.execute("select count(*) from zillow"))
 api = zillow.ValuationApi()
 # key = "X1-ZWz17fdl35adqj_3bebs"
 key = zillow_key
+app.config['SECRET_KEY'] = 'awssecreykey123'
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+
 
             
 
@@ -43,39 +55,32 @@ def zillow_call():
 
     return render_template('index.html', data=results)
 ########Signup Froms 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    username = db.Column(db.String(50))
-    socialsecurity = db.Column(db.String(50))
-    email = db.Column(db.String(50))
-    phonenumber = db.Column(db.Integer)
-    gender = db.Column(db.String(50))
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+User = Base.classes.user_data    
+
+
   
 
 
 class RegisterForm(FlaskForm):
     name = StringField('name', validators=[InputRequired(), Length(max=50)])
-    username = StringField('address', validators=[InputRequired(), Length(min=1, max=8)])
+    address = StringField('address', validators=[InputRequired(), Length(min=1, max=8)])
     socialsecurity = StringField('socialsecurity', validators=[InputRequired(), Length(min=1, max=8)])
     email = StringField('email', validators=[InputRequired(), Length(min=1, max=8)])
     phonenumber = IntegerField('phonenumber', validators=[InputRequired(), Length(min=1, max=8)])
     gender = StringField('gender', validators=[InputRequired(), Length(min=1, max=8)])
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegisterForm()
-
-    if form.validate_on_submit():
-        new_user = User(name=form.name.data, username=form.username.data,socialsecurity=form.socialsecurity.data,email=form.email.data,phonenumber=form.phonenumber.data,gender=form.gender.data)
+    if request.method == 'POST': 
+        new_user = User(name=form.name.data, address=form.address.data,socialsecurity=form.socialsecurity.data,email=form.email.data,phonenumber=form.phonenumber.data,gender=form.gender.data)
         db.session.add(new_user)
         db.session.commit()
-        return '<h1>New user has been created!</h1>'
-        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
-
     return render_template('signup.html', form=form)
-
 
 
 if __name__ == "__main__":
