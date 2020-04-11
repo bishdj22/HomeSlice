e4598c36652f1e9012e5d89733e8081fa059c877
cmokaya94 <56549200+cmokaya94@users.noreply.github.com>
2020-04-11 12:18:25 -0400

    signup forms

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, redirect
 from calculations import address_search
 from config import zillow_key, database_key
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField,IntegerField
+
 
 
 # Create an instance of Flask
@@ -19,6 +22,7 @@ engine = create_engine(f'postgresql://{connection_string}')
 api = zillow.ValuationApi()
 # key = "X1-ZWz17fdl35adqj_3bebs"
 key = zillow_key
+db = SQLAlchemy(app)
 
             
 
@@ -38,6 +42,40 @@ def zillow_call():
         
 
     return render_template('index.html', data=results)
+########Signup Froms 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    username = db.Column(db.String(50))
+    socialsecurity = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    phonenumber = db.Column(db.Integer)
+    gender = db.Column(db.String(50))
+  
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('name', validators=[InputRequired(), Length(max=50)])
+    username = StringField('address', validators=[InputRequired(), Length(min=1, max=8)])
+    socialsecurity = StringField('socialsecurity', validators=[InputRequired(), Length(min=1, max=8)])
+    email = StringField('email', validators=[InputRequired(), Length(min=1, max=8)])
+    phonenumber = IntegerField('phonenumber', validators=[InputRequired(), Length(min=1, max=8)])
+    gender = StringField('gender', validators=[InputRequired(), Length(min=1, max=8)])
+
+
+@app.route('/', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        new_user = User(name=form.name.data, username=form.username.data,socialsecurity=form.socialsecurity.data,email=form.email.data,phonenumber=form.phonenumber.data,gender=form.gender.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return '<h1>New user has been created!</h1>'
+        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
+
 
 
 if __name__ == "__main__":
